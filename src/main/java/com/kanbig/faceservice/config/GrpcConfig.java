f8e8417b052e62754bee09a781a4d42fc4dfef7e
刘万杰 <crab_liu@yunzhijia.com>
2020-12-09 20:03:06 +0800

    update

--- src/main/java/com/kanbig/faceservice/config/GrpcConfig.java
+++ src/main/java/com/kanbig/faceservice/config/GrpcConfig.java
@@ -2,6 +2,9 @@ package com.kanbig.faceservice.config;
 
 import java.util.concurrent.TimeUnit;
 
+import javax.annotation.PreDestroy;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,7 +13,9 @@ import com.kanbig.faceservice.KanbigImageServiceGrpc;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Configuration
 public class GrpcConfig {
 
@@ -19,16 +24,27 @@ public class GrpcConfig {
 	@Value("${grpc.host:50051}")
 	private int grpcPort;
 
+	@Autowired
+	private ManagedChannel channel;
+
 	@Bean
 	public ManagedChannel grpcChannel() {
-		return ManagedChannelBuilder.forAddress(grpcHost, grpcPort).usePlaintext()
-				.idleTimeout(3, TimeUnit.SECONDS)
+		return ManagedChannelBuilder.forAddress(grpcHost, grpcPort).usePlaintext().idleTimeout(3, TimeUnit.SECONDS)
 				.build();
 	}
 
+	@PreDestroy
+	public void shutdown() {
+		try {
+			channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+			log.info("ManagedChannel ------ shutdown success");
+		} catch (Exception e) {
+			log.warn("ManagedChannel ------ shutdown fail");
+		}
+	}
+
 	@Bean
-	public KanbigImageServiceGrpc.KanbigImageServiceBlockingStub kanbigImageServiceBlockingStub(
-			ManagedChannel channel) {
+	public KanbigImageServiceGrpc.KanbigImageServiceBlockingStub kanbigImageServiceBlockingStub() {
 		return KanbigImageServiceGrpc.newBlockingStub(channel);
 	}
 }
