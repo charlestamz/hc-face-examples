2d7bb54c7d4cb66eed48de72cc8c2148e86e6400
刘万杰 <crab_liu@yunzhijia.com>
2020-12-10 11:46:20 +0800

    update

--- src/main/java/com/kanbig/faceservice/config/GrpcConfig.java
+++ src/main/java/com/kanbig/faceservice/config/GrpcConfig.java
@@ -1,5 +1,8 @@
 package com.kanbig.faceservice.config;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 import javax.annotation.PreDestroy;
@@ -27,14 +30,16 @@ public class GrpcConfig {
 	@Autowired
 	private ManagedChannel channel;
 
-	
 	@Bean
 	public ManagedChannel grpcChannel() {
 		/**
-		 * idleTimeout 空闲超时
-		 * enableRetry 开启重试，当服务端重启后，客户端可以重连上
+		 * idleTimeout 空闲超时 enableRetry 开启重试，当服务端重启后，客户端可以重连上
 		 */
-		return ManagedChannelBuilder.forAddress(grpcHost, grpcPort).usePlaintext().idleTimeout(3, TimeUnit.SECONDS).enableRetry()
+		return ManagedChannelBuilder.forAddress(grpcHost, grpcPort).usePlaintext().idleTimeout(3, TimeUnit.SECONDS)
+				.enableRetry()
+				.executor(new ThreadPoolExecutor(0, 3, 60L, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(100),
+						// 抛弃当前的任务
+						new ThreadPoolExecutor.CallerRunsPolicy()))
 				.build();
 	}
 
